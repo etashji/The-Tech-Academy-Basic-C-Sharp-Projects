1f32e3ee0d4d86ca5a86b5fcd35a95e046edb5a2
etashji <99293073+etashji@users.noreply.github.com>
2022-03-04 14:17:13 -0500

    Welcome Window

---
+++ Welcome Screen/Welcome Screen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Welcome_Screen
+{
+    class Program
+    {
+        static void Main()
+        { 
+            Console.Write("Welcome to Acme Accounting Systems! \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+
+    }    
+}
