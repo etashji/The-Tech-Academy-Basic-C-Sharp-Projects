85d46df3e83a1074f750e2879d89b0c6420687bd
etashji <99293073+etashji@users.noreply.github.com>
2022-03-04 16:30:41 -0500

    Added MathChallenge and MathAndComparisonOperators

---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1 = 5;
+            decimal num2 = 7;
+            decimal addition = num1 + num2;
+            decimal subtraction = num2 - num1;
+            decimal multiplication = num1 * num2;
+            decimal division = num1 / num2;
+            Console.WriteLine(addition);
+            Console.WriteLine(subtraction);
+            Console.WriteLine(multiplication);
+            Console.WriteLine(division);
+            Console.WriteLine("Hello" + "World!");
+
+            Console.Read();
+        }
+    }
+}
