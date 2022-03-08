1da9f61179c8547a7e48ee67be6dd8dd66ebeb5f
etashji <99293073+etashji@users.noreply.github.com>
2022-03-08 13:19:37 -0500

    IterationExercises Part 4

---
+++ IterationExercise/IterationExercise/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] verbs = { "Find", "Lick", "Eat", "Drink" };
+        Console.WriteLine("What three letters do you add to the end of a verb to change it from simple present tense to progressive tense?");
+        string ending = Console.ReadLine();
+        bool boolean = true;
+
+
+        for (int j = 0; j < verbs.Length; j++)
+        {
+            verbs[j] = verbs[j] + ending;
+            Console.WriteLine(verbs[j]);
+
+        } while (boolean)
+        {
+            Console.WriteLine("To infinity and beyond!");
+            boolean = false;
+        } for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Less than Loop");
+        } for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("Less than or equal to loop");
+        }
+
+        List<string> cars = new List<string>() { "Volvo", "BMW", "Ford", "Mazda" };
+        Console.WriteLine("What is your favorite brand of car?");
+        string response = Console.ReadLine();
+        bool indexBool = false;
+
+        foreach (string car in cars)
+        { 
+            if (car == response)
+            {
+                int index = cars.IndexOf(car);
+                do
+                {
+                    switch (index)
+                    {
+                        case 0:
+                            Console.WriteLine("Your car is located at index: " + index);
+                            indexBool = true;
+                            break;
+                        case 1:
+                            Console.WriteLine("Your car is located at index: " + index);
+                            indexBool = true;
+                            break;
+                        case 2:
+                            Console.WriteLine("Your car is located at index: " + index);
+                            indexBool = true;
+                            break;
+                        case 3:
+                            Console.WriteLine("Your car is located at index: " + index);
+                            indexBool = true;
+                            break;
+                        default:
+                            Console.WriteLine("Your car is not on the list.");
+                            break;
+                    }
+                } while (!indexBool);
+            } 
+        } Console.ReadLine();
+    } 
+}
