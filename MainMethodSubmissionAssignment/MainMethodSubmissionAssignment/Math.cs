9f6fc22ea6f61e1497ff5598477a71b9f6e73b62
etashji <99293073+etashji@users.noreply.github.com>
2022-03-09 15:25:38 -0500

    I completed the Main Method Submission Assignment

---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Math
+    {
+        public int PlusOne(int input) //This creates the PlusOne method and assigns it the input integer.
+        {
+            int answer = input + 1; //This creates the answer integer by adding 1 to the input integer.
+            return answer; //This ensures the method returns the answer integer.
+        }
+        public int PlusOne(decimal decimals) //This creates the PlusOne method and assigns it the decimals decimal.
+        {
+            decimal answer = decimal.Add(decimals, 1.0m); //This creates the answer decimal by adding 1 to the decimals decimal.
+            return Convert.ToInt32(answer); //This ensures the method returns the answer integer.
+        }
+        public int PlusOne(string strings) //This creates the PlusOne method and assigns it the strings string.
+        {
+            int integer = Convert.ToInt32(strings); //This converts the strings string to an integer.
+            int answer = integer + 1; //This creates the answer integer by adding 1 to the integer integer.
+            return answer; //This ensures the method returns the answer integer.
+        } 
+    }
+}
---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); //This instantiates the class
+            
+            int input = 1; //This creates the input integer
+            int answer =  math.PlusOne(input); //This asks the console to perform the PlusOne method from the math Math class on the input integer and assigns it to the answer integer.
+            Console.WriteLine(answer); //This asks the console to display the answer integer.
+
+            decimal decimals = 0.5m; //This creates the decimals decimal
+            int answer2 = math.PlusOne(decimals);//This asks the console to perform the PlusOne method from the math Math class on the decimals decimal and assigns it to the answer2 integer.
+            Console.WriteLine(answer2); //This asks the console to display the answer integer.
+
+            string strings = "1"; //This creates the strings string.
+            int answer3 = math.PlusOne(strings); //This asks the console to perform the PlusOne method from the math Math class on the strings string and assigns it to the answer3 integer.
+            Console.WriteLine(answer3); //This asks the console to display the answer integer.
+
+            Console.ReadLine(); //This ensures the console stays open.
+        }
+    }
+}
