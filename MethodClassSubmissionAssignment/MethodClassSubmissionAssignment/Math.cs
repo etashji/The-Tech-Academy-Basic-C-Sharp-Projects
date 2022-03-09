fe4ad137952cda0528f72f18930a65bc6f7735c4
etashji <99293073+etashji@users.noreply.github.com>
2022-03-09 16:24:07 -0500

    Completed the Method Class Submission Assignment

---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Math
+    {
+        public void Addition(int integer1, int integer2) //This declares the Addition method with the integer1 and integer2 integers.
+        {
+            int sum = integer1 + 1; //This declares the sum integer as integer + 1
+            Console.WriteLine("Sum: " + sum + " Second integer: " + integer2); //This writes the sum and integer2 integers.
+            return; //This displays the result.
+        }
+    }
+}
---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); //This instantiates the math class as a Math class.
+            int integer1 = 1; //This declares and defines the integer integer1.
+            int integer2 = 2; //This declares and defines the integer integer2.
+
+            math.Addition(1, 2); //This performs the math.Addition method with the parameter of 1 and 2.
+            math.Addition(integer1, integer2); //This performs the math.Addition method with the parameter of integer1 and integer2
+            Console.ReadLine();//This ensures the console stays open.
+        }
+    }
+}
