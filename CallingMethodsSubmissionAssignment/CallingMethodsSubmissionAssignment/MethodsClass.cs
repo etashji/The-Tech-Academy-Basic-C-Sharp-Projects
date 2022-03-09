fa59c2d7d6897c6abb4b48f5ef556702269de876
etashji <99293073+etashji@users.noreply.github.com>
2022-03-09 14:40:07 -0500

    CallingMethodsSubmissionAssignment

---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/MethodsClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    public class MethodsClass
+    {
+        public int PlusOne(int input) //This declares the PlusOne method with the input integer
+        {
+            int answer = input + 1; //This creates the answer integer by adding 1 to the input integer.
+            return answer; //This returns the answer integer.
+        }
+        public int MinusOne(int input) //This declares the MinusOne method with the input integer
+        {
+            int answer = input - 1; //This creates the answer integer by subtracting 1 from the input integer.
+            return answer;//This returns the answer integer.
+        }
+        public int TimesTen(int input) //This declares the MinusOne method with the input integer
+        {
+            int answer = input * 10; //This creates the answer integer by multiplying 10 times the input integer.
+            return answer; //This returns the answer integer.
+        }
+    }
+    
+}
---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which number would you like to use?"); //This asks the user what number they would like to use.
+            int input = Convert.ToInt32(Console.ReadLine()); //This saves the user's input as the input integer.
+            MethodsClass class1 = new MethodsClass(); //This creates the class1 MethodsClass.
+
+            int addition = class1.PlusOne(input); //This declares the addition integer by using the PlusOne method from the class1 MethodsClass class.
+            int subtraction = class1.MinusOne(input); //This declares the subtraction integer by using the MinusOne method from the class1 MethodsClass class.
+            int multiplication = class1.TimesTen(input); //This declares the multiplication integer by using the TimesTen method from the class1 MethodsClass class.
+            Console.WriteLine(input + " plus one " + "equals " + addition); //This displays the answer from the PlusOne method.
+            Console.WriteLine(input + " minus one " + "equals " + subtraction); //This displays the answer from the MinusOne method.
+            Console.WriteLine(input + " times ten " + "equals " + multiplication); //This displays the answer from the TimesTen method.
+            Console.ReadLine();//This ensures the console stays open.
+
+        }
+        
+    }
+}
