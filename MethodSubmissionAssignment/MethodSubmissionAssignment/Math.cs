9afd449d336d3421f419b527aaeeef6d49c46155
etashji <99293073+etashji@users.noreply.github.com>
2022-03-09 16:08:51 -0500

    Method Submission Assignment (in progress)

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Math
+    {
+        public int Addition(int integer, int optional = 0)
+        {
+            int answer = integer + optional + 1;
+            return answer;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+                Math math = new Math();
+                Console.WriteLine("What is your first number? ");
+                int input1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your second number? Note: this number is optional.");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+
+
+                int answer = math.Addition(input1); 
+                Console.WriteLine(answer);
+                Console.ReadLine();
+        }
+    }
+}
