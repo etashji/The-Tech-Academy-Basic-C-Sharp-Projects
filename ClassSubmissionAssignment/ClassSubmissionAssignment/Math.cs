16b992e446b90ab337cd8273f9de1a94777fcfc1
etashji <99293073+etashji@users.noreply.github.com>
2022-03-09 19:05:36 -0500

    Completed Class Submission Assignment

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Math
+    {
+        public void Division(int input)
+        {
+            Console.WriteLine(input / 2);
+            return;
+        }
+
+        public int Division(int input1, int input2)
+        {
+            int answer = input1 / input2;
+            return answer;
+        }
+
+        public void Output(out int Output1, out int Output2)
+        {
+            Output1 = 44;
+            Output2 = 45;
+        }
+
+        public static void Static()
+        {
+            Console.WriteLine("This is my static class.");
+        }
+        public void Static2()
+        {
+            Static();
+        }
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please ente a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int Output1 = 0;
+            int Output2 = 0;
+            int input1 = 8;
+            int input2 = 4;
+            
+            Console.WriteLine("Your number divided by 2 equals: ");
+            math.Division(input);
+
+            Console.WriteLine("My output parameters are: ");
+            math.Output(out Output1, out Output2);
+            Console.WriteLine(Output1 + " and " + Output2);
+
+            Console.WriteLine("The difference in my overloaded method is: ");
+            Console.WriteLine(math.Division(input1, input2));
+
+            Console.WriteLine("The output for my static class is: ");
+            math.Static2();
+
+            Console.ReadLine();
+        }   
+
+    }
+}
