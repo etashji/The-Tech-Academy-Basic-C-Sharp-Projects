e9f4aa846e1a9a5cbba2298a0becef9bd71433de
etashji <99293073+etashji@users.noreply.github.com>
2022-03-10 13:14:37 -0500

    Completed the Operators Submission Assignment

---
+++ OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    public class Employee
+    {
+        public string firstName { get; set; } //This sets the firstName property for the Employee class.
+        public string lastName { get; set; } //This sets the lastName property for the Employee class.
+
+        public int Id { get; set; } //This sets the Id property in the Employee class.
+
+        public override bool Equals(object obj) //This is an override so the == operator can be overloaded by Employee.
+        {
+            return Equals(obj as Employee); //This works with the above statement.
+        }
+
+
+        public override int GetHashCode() //This overrides the GetHashCode so the == operater can be overloaded by Employee.
+        {
+            return 2108858624 + Id.GetHashCode(); //This provides a default number to assist the above statement.
+        }
+
+        public static Employee operator== (Employee employee1, Employee employee2) //This overloads the == operator.
+        {
+            employee1.Id.Equals(employee2.Id); //This displays whether the Id for employee1 and employee2 are true.
+            return employee1; //This returns the result.
+        }
+
+        public static Employee operator != (Employee employee1, Employee employee2) //This overloads the != operator.
+        {
+            employee1.Id.Equals(employee2.Id); //This displays whether the id for employee1 and employee2 are equal.
+            return employee1; //This returns the result.
+        }
+    }
+}
---
+++ OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); //This instantiates the Employee class and creates the employee1 object.
+            Employee employee2 = new Employee(); //This creates the employee 2 object.
+
+            employee1.Id = 12345; //This defines the Id property for employee1.
+            employee2.Id = 12345; //This defines the Id property for employee2.
+
+            Console.WriteLine("It is:"); //This writes "It is:" to the console.
+            Console.WriteLine(employee1.Id == employee2.Id); //This determines whether the Id properties for employee1 and employee2 are the same.
+            Console.WriteLine("that employee1 is equal to employee 2."); //This writes "that employee1 is equal to employee2."
+            Console.ReadLine(); //This ensures the console stays open.
+        }
+    }
+}
