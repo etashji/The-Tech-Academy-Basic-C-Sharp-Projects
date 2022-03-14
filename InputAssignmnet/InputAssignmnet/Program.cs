e049bdd127a140009e1454db962385c23cb816eb
etashji <99293073+etashji@users.noreply.github.com>
2022-03-14 08:48:03 -0400

    Completed the Input Submission Assignment

---
+++ InputAssignmnet/InputAssignmnet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignmnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your number?");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Eric Tashji\Logs\input.txt", "Your number is: " + text);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Eric Tashji\Logs\input.txt"));
+            Console.ReadLine();
+        }
+    }
+}
