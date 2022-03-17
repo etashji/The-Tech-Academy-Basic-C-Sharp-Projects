2a23b7b3099c5b09bf9794c2a7b812407f3ee7e6
etashji <99293073+etashji@users.noreply.github.com>
2022-03-17 18:31:56 -0400

    Completed the Finall Challenge Submission Assignment

---
+++ FinalChallengeSubmissionAssignment/FinalChallengeSubmissionAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace FinalChallengeSubmissionAssignment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                Console.WriteLine("Please enter the student's first name: ");
+                var firstName = Console.ReadLine();
+
+                Console.WriteLine("Please enter the student's last name: ");
+                var lastName = Console.ReadLine();
+
+                var student = new Students { FirstName = firstName, LastName = lastName };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from b in db.Students
+                            orderby b.LastName
+                            select b;
+                Console.WriteLine("All students in the database: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ FinalChallengeSubmissionAssignment/FinalChallengeSubmissionAssignment/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace FinalChallengeSubmissionAssignment
+{
+    class StudentContext : DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+    }
+}
