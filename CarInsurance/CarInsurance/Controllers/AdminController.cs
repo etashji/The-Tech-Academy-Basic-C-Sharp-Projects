637db01d3b8960acb208e5da21fd4031c541cc6a
etashji <99293073+etashji@users.noreply.github.com>
2022-03-17 16:51:26 -0400

    Completed Car Insurance

--- CarInsurance/CarInsurance/Controllers/AdminController.cs
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -9,16 +9,81 @@ using System.Web.Mvc;
 using CarInsurance.Models;
 using CarInsurance.ViewModels;
 
+
 namespace CarInsurance.Controllers
 {
     public class AdminController : Controller
     {
+        private InsuranceEntities db = new InsuranceEntities();
+        public decimal GetQuote(Insuree insuree)
+        {
+            insuree.Quote = 50;
+            bool hadBirthdayMonth = false;
+            bool hadBirthdayDay = false;
+            DateTime insureeDOB = Convert.ToDateTime(insuree.DateOfBirth);
+            int insureeAge = 0;
+            if (insureeDOB.Month <= DateTime.Now.Month)
+            {
+                hadBirthdayMonth = true;
+            }
+            if (insureeDOB.Day <= DateTime.Now.Day)
+            {
+                hadBirthdayDay = true;
+            }
+            if (hadBirthdayDay == true && hadBirthdayMonth == true)
+            {
+                insureeAge = DateTime.Now.Year - insureeDOB.Year;
+            }
+            else
+            {
+                insureeAge = DateTime.Now.Year - insureeDOB.Year - 1;
+            }
+            if (insureeAge <= 18)
+            {
+                insuree.Quote += 100;
+            }
+            else if (insureeAge > 19 && insureeAge < 25)
+            {
+                insuree.Quote += 50;
+            }
+            else
+            {
+                insuree.Quote += 25;
+            }
+            if (insuree.CarYear < 2000)
+            {
+                insuree.Quote += 25;
+            }
+            else if (insuree.CarYear > 2015)
+            {
+                insuree.Quote += 25;
+            }
+            if (insuree.CarMake == "Porsche")
+            {
+                insuree.Quote += 25;
+
+                if (insuree.CarModel == "911 Carrera")
+                {
+                    insuree.Quote += 25;
+                }
+            }
+            insuree.Quote += insuree.SpeedingTickets * 10;
+            if (insuree.DUI == true)
+            {
+                insuree.Quote = decimal.Multiply(insuree.Quote, 1.25m);
+            }
+            if (insuree.CoverageType == true)
+            {
+                insuree.Quote = decimal.Multiply(insuree.Quote, 1.5m);
+            }
+            return (insuree.Quote);
+        }
         public ActionResult Index()
         {
             using (InsuranceEntities db = new InsuranceEntities())
             {
                 var insurees = (from c in db.Insurees select c).ToList();
-                IEnumerable<InsureeVm> insureeVms = new List<InsureeVm>();
+                List<InsureeVm> insureeVms = new List<InsureeVm>();
                 foreach (var insuree in insurees)
                 {
                     var insureeVm = new InsureeVm();
@@ -26,12 +91,46 @@ namespace CarInsurance.Controllers
                     insureeVm.FirstName = insuree.FirstName;
                     insureeVm.LastName = insuree.LastName;
                     insureeVm.EmailAddress = insuree.EmailAddress;
-                    insureeVms.MoveNext(insureeVm);
+                    insureeVm.Quote = insuree.Quote;
+                    insureeVms.Add(insureeVm);
                 }
                 return View(insureeVms);
             }
             
         }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+
+
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = GetQuote(insuree);
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
 
     }
 }
